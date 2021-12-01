cc36196193e70a8561c9a4297326e9b79e4abac1
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:24 -0500

    [UPD] - Betty style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,19 +3,24 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: Principal Node
+ * @value: Value to new node in the right of the parent Node
+ * Return: Address of the new child node
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *child = NULL;
+	binary_tree_t *child = NULL;
 
-    child = binary_tree_node(parent, value);
-    if (!parent->right)
-        parent->right = child;
-    else
-    {
-        child->right = parent->right;
-        parent->right->parent = child;
-        parent->right = child;
-    }
-
-    return (child);
+	child = binary_tree_node(parent, value);
+	if (!parent->right)
+		parent->right = child;
+	else
+	{
+		child->right = parent->right;
+		parent->right->parent = child;
+		parent->right = child;
+	}
+	return (child);
 }
