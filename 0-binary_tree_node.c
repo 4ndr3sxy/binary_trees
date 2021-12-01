3e48b2523e66d54f33b835e6288f0d20ab0c8454
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:14 -0500

    [UPD] - Betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,17 +3,23 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Create a child to a any parent
+ * @parent: Principal Node
+ * @value: The value to the child node
+ * Return: The address of the new Node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *child = NULL;
+	binary_tree_t *child = NULL;
 
-    child = malloc(sizeof(binary_tree_t));
-    if (!child)
-        return (NULL);
-    child->left = NULL;
-    child->right = NULL;
-    child->parent = parent;
-    child->n = value;
+	child = malloc(sizeof(binary_tree_t));
+	if (!child)
+		return (NULL);
+	child->left = NULL;
+	child->right = NULL;
+	child->parent = parent;
+	child->n = value;
 
-    return (child);
+	return (child);
 }
