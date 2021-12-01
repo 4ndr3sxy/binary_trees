817f5753192cb1532bd46b23a62b6fee402807af
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 15:18:03 -0500

    [FIX] - adjustment in logic, names of the functions and conditionals

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,12 +10,12 @@
  * @func: Function pointer
  * Return:Without return
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder_s(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree->left)
-		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder_s(tree->left, func);
 	if (tree->right)
-		binary_tree_postorder(tree->right, func);
+		binary_tree_postorder_s(tree->right, func);
 	func(tree->n);
 }
 
@@ -25,8 +25,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
  * @func: Function pointer
  * Return: Without return
  */
-void binary_tree_binary_tree_postorder(const binary_tree_t *tree,
-									   void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree,
+						   void (*func)(int))
 {
 	if (tree && func)
 		binary_tree_postorder(tree, func);
