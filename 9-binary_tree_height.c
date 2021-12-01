8280769b8d8f23e33562639d173be3dd1249582f
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:53 -0500

    [UPD] - Betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,54 +3,29 @@
 #include <string.h>
 #include "binary_trees.h"
 
-size_t binary_tree_height_s_r(const binary_tree_t *tree)
+/**
+ * height - Measures the height of a binary tree
+ * @tree: Pointer to the node to measures the height
+ * Return: The height of the tree starting at @node
+ */
+size_t height(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return 0;
-    }
-    if (tree->right)
-    {
-        return 1 + (binary_tree_height_s_r(tree->right));
-    }
-    else if (tree->left)
-    {
-        return 1 + (binary_tree_height_s_r(tree->left));
-    }
-    else
-        return 0;
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + height(tree->left) : 0;
+	height_r = tree->right ? 1 + height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
 
-size_t binary_tree_height_s_l(const binary_tree_t *tree)
-{
-    if (!tree)
-    {
-        return 0;
-    }
-    if (tree->left)
-    {
-        return 1 + (binary_tree_height_s_l(tree->left));
-    }
-    else if (tree->right)
-    {
-        return 1 + (binary_tree_height_s_l(tree->right));
-    }
-    else
-        return 0;
-}
-
+/**
+ * binary_tree_height - Validate if tree is not null
+ * @tree: Binary tree
+ * Return: 0 if the tree is NULL or the value the return height.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t result_l = 0;
-    size_t result_r = 0;
-    if (tree)
-    {
-        result_l = binary_tree_height_s_l(tree);
-        result_r = binary_tree_height_s_r(tree);
-        if (result_l > result_r)
-            return result_l;
-        else
-            return result_r;
-    }
-    return (0);
+	if (tree)
+		return (height(tree));
+	return (0);
 }
