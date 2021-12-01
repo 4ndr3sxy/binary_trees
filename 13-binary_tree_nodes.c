4f30fedb20e3aa4b7fa0a4653cbb3cba6ed343c9
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:45:10 -0500

    [UPD] - Betty style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,67 +3,42 @@
 #include <string.h>
 #include "binary_trees.h"
 
-// size_t binary_tree_height_s_r(const binary_tree_t *tree)
-// {
-//     if (!tree)
-//     {
-//         return 0;
-//     }
-//     if (tree->right)
-//     {
-//         return 1 + (binary_tree_height_s_r(tree->right));
-//     }
-//     else if (tree->left)
-//     {
-//         return 1 + (binary_tree_height_s_r(tree->left));
-//     }
-//     else
-//         return 0;
-// }
-
-// size_t binary_tree_height_s_l(const binary_tree_t *tree)
-// {
-//     if (!tree->left && !tree->right)
-//     {
-//         return (1);
-//     }
-//     if (tree->left)
-//     {
-//         return (0 + binary_tree_height_s_l(tree->left));
-//     }
-//     if (tree->right)
-//     {
-//         return (0 + binary_tree_height_s_l(tree->right));
-//     }
-//     return 0;
-// }
-
+/**
+ * binary_tree_height_s_l - counts the nodes with at least
+ * @tree: Pointer to the node to measures the height
+ * Return: counts the nodes with at least
+ */
 size_t binary_tree_height_s_l(const binary_tree_t *tree)
 {
-    size_t left;
-    size_t right;
+	size_t left;
+	size_t right;
 
-    if (tree->left || tree->right)
-    {
-        left = tree->left ? 1 + binary_tree_height_s_l(tree->left) : 0;
-        right = tree->right ? 1 + binary_tree_height_s_l(tree->right) : 0;
-        return (left + right);
-    }
-    else
-    {
-        return (0);
-    }
+	if (tree->left || tree->right)
+	{
+		left = tree->left ? 1 + binary_tree_height_s_l(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height_s_l(tree->right) : 0;
+		return (left + right);
+	}
+	else
+		return (0);
 }
 
+/**
+ * binary_tree_nodes - Validate if the tree is not null
+ * 1 child in a binary tree
+ * @tree: Binary tree
+ * Return: counts the nodes with at least
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t result_l = 0;
-    if (tree)
-    {
-        result_l = binary_tree_height_s_l(tree);
-        if(!tree->parent)
-            result_l--;
-        return (result_l);
-    }
-    return (0);
+	size_t result_l = 0;
+
+	if (tree)
+	{
+		result_l = binary_tree_height_s_l(tree);
+		if (!tree->parent)
+			result_l--;
+		return (result_l);
+	}
+	return (0);
 }
