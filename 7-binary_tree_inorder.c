f84eb8968b9200a90f978e07d7150b6e0c3bcfc3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:44 -0500

    [UPD] - Betty style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,23 +3,29 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * inorder - function that print binary tree using in-order traversal
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return:Without return
+ */
 void inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree->left)
-    {
-        inorder(tree->left, func);
-    }
-    func(tree->n);
-    if (tree->right)
-    {
-        inorder(tree->right, func);
-    }
+	if (tree->left)
+		inorder(tree->left, func);
+	func(tree->n);
+	if (tree->right)
+		inorder(tree->right, func);
 }
 
+/**
+ * binary_tree_inorder - Validate if the tree is not null
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return: Without return
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree)
-    {
-        inorder(tree, func);
-    }
+	if (tree)
+		inorder(tree, func);
 }
