675fa63fe1cfbd279bf229ec24363b9560a0c381
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:54:13 -0500

    [UPD] - edge case of function called, if return (NULL)

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *child = NULL;
 
 	child = binary_tree_node(parent, value);
+	if (!child)
+		return (NULL);
 	if (!parent->left)
 		parent->left = child;
 	else
