562766210d35e025e692be8a141fd5ca1448e819
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 15:38:12 -0500

    [FIX] - Fix code in conditional to edge case result = 0

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -36,7 +36,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree)
 	{
 		result_l = binary_tree_height_s_l(tree);
-		if (!tree->parent)
+		if (!tree->parent && result_l != 0)
 			result_l--;
 		return (result_l);
 	}
