56a67910eabdb15b2e7dba91a01e0044a49882c3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:48 -0500

    [UPD] - Betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,23 +3,31 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - function that print binary tree using
+ * post-order traversal
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return:Without return
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree->left)
-    {
-        binary_tree_postorder(tree->left, func);
-    }
-    if (tree->right)
-    {
-        binary_tree_postorder(tree->right, func);
-    }
-    func(tree->n);
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
 
-void binary_tree_binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+/**
+ * binary_tree_binary_tree_postorder - Validate if the tree is not null
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return: Without return
+ */
+void binary_tree_binary_tree_postorder(const binary_tree_t *tree,
+									   void (*func)(int))
 {
-    if (tree)
-    {
-        binary_tree_postorder(tree, func);
-    }
+	if (tree)
+		binary_tree_postorder(tree, func);
 }
