48d3cb1d771df966781e73558f063aa84c53f50e
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:28 -0500

    [UPD] - Betty style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,19 +3,29 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * delete - delete any nodes in the binary tree with recursion
+ * @tree: Binary tree
+ * Return: Without return
+ */
 void delete(binary_tree_t *tree)
 {
-    if (tree->right)
-        delete(tree->right);
-    if (tree->left)
-        delete(tree->left);
-    free(tree);
+	if (tree->right)
+		delete(tree->right);
+	if (tree->left)
+		delete(tree->left);
+	free(tree);
 }
 
+/**
+ * binary_tree_delete - Principal function
+ * @tree: Binary tree
+ * Return: Without return
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        delete(tree);
-    }
+	if (tree)
+	{
+		delete(tree);
+	}
 }
