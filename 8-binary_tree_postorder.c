3125d3e7f2d57e2c07ac57bf88ccbb97486cc6f6
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 15:29:13 -0500

    [FIX] - name of function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -29,5 +29,5 @@ void binary_tree_postorder(const binary_tree_t *tree,
 						   void (*func)(int))
 {
 	if (tree && func)
-		binary_tree_postorder(tree, func);
+		binary_tree_postorder_s(tree, func);
 }
