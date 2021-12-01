0076d48206a825210274aa0b42c1415c6830e6e5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:44:41 -0500

    [UPD] - Betty style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,19 +3,31 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * preorder - function that print binary tree using pre-order traversal
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return:Without return
+ */
 void preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    func(tree->n);
-    if (tree->left)
-        preorder(tree->left, func);
-    if (tree->right)
-        preorder(tree->right, func);
+	func(tree->n);
+	if (tree->left)
+		preorder(tree->left, func);
+	if (tree->right)
+		preorder(tree->right, func);
 }
 
+/**
+ * binary_tree_preorder - Validate if the tree is not null
+ * @tree: Binary tree
+ * @func: Function pointer
+ * Return: Without return
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree)
-    {
-        preorder(tree, func);
-    }
+	if (tree)
+	{
+		preorder(tree, func);
+	}
 }
