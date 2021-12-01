dd49890c9be7649ee1c17c58063271eeeecaf5a6
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-01 14:45:15 -0500

    [UPD] - Betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,23 +3,11 @@
 #include <string.h>
 #include "binary_trees.h"
 
-size_t binary_tree_height_s_l(const binary_tree_t *tree)
-{
-    size_t left;
-    size_t right;
-
-    if (tree->left || tree->right)
-    {
-        left = tree->left ? 1 + binary_tree_height_s_l(tree->left) : 0;
-        right = tree->right ? 1 + binary_tree_height_s_l(tree->right) : 0;
-        return (left + right);
-    }
-    else
-    {
-        return (0);
-    }
-}
-
+/**
+ * height - measures the balance factor of a binary tree
+ * @tree: Binary tree
+ * Return: measures the balance factor of a binary tree
+ */
 size_t height(const binary_tree_t *tree)
 {
 	size_t height_l;
@@ -30,13 +18,19 @@ size_t height(const binary_tree_t *tree)
 	return (height_l - height_r);
 }
 
+/**
+ * binary_tree_balance - Validate that the tree is not null
+ * @tree: Binary tree
+ * Return: measures the balance factor of a binary tree
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t result_l = 0;
-    if (tree)
-    {
-        result_l = height(tree);
-        return (result_l);
-    }
-    return (0);
+	size_t result_l = 0;
+
+	if (tree)
+	{
+		result_l = height(tree);
+		return (result_l);
+	}
+	return (0);
 }
