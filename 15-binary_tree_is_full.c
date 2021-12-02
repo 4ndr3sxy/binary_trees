c10dc31204ab7dd87e4967396d3c1a0c2d07c574
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-12-02 10:10:05 -0500

    [FIX] - Fix Code, lagic changed

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,46 +4,19 @@
 #include "binary_trees.h"
 
 /**
- * _pow - get pow(x, y) with recursion
- * @x: base
- * @y: elevation
- * Return: pow of number x elevator in y
- */
-size_t _pow(size_t x, size_t y)
-{
-	if (y > 1)
-	{
-		x *= 2;
-		y--;
-		return (_pow(x, y));
-	}
-	return (x);
-}
-
-/**
  * height - get count of N in  abinary tree depend of validator
  * @tree: Binary tree
- * @validator: flag
  * Return: number of N depend of validator
  */
-size_t height(const binary_tree_t *tree, int validator)
+size_t height(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
-
-	height_l = tree->left ? 1 + height(tree->left, validator) : 0;
-	height_r = tree->right ? 1 + height(tree->right, validator) : 0;
-	switch (validator)
-	{
-	case 0:
-		return (height_r - height_l);
-	case 1:
-		return (height_l > height_r ? height_l : height_r);
-	case 2:
-		return (height_l + height_r);
-	default:
+	if (!tree)
 		return (0);
-	}
+	if ((!tree->left && tree->right) || ((tree->left && !tree->right)))
+		return (0);
+	if ((tree->left && tree->right))
+		return (height(tree->left) * height(tree->right));
+	return (1);
 }
 
 /**
@@ -54,25 +27,11 @@ size_t height(const binary_tree_t *tree, int validator)
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	size_t resultHeight = 0;
-	size_t resultLevels = 0;
-	size_t resultCountNodes = 0;
-	size_t resultPow = 0;
 
 	if (tree)
 	{
-		resultHeight = height(tree, 0);
-		if (resultHeight == 0)
-		{
-			resultCountNodes = height(tree, 2);
-			resultCountNodes += 2;
-			resultLevels = height(tree, 1);
-			resultLevels++;
-			resultPow = _pow(2, resultLevels);
-			if (resultPow == resultCountNodes)
-			{
-				return (1);
-			}
-		}
+		resultHeight = height(tree);
+		return (resultHeight);
 	}
 	return (0);
 }
